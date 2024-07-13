24be0acb51ca6ba1da11392ddb410c30ef3fbcab
JordanSlater <32441504+JordanSlater@users.noreply.github.com>
2024-07-13 16:24:17 -0400

    Add card centered at bottom of scene

---
+++ script/hand.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class hand : Control
+{
+	[Export]
+	public PackedScene cardScene;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		AddCard();
+	}
+
+	public void AddCard()
+	{
+		var instance = cardScene.Instantiate();
+		AddChild(instance);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
